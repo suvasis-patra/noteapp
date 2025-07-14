d8126063e6e18f0d07c2c4daafef77207e3376fa
suvasis-patra <suvasis02@kgpian.iitkgp.ac.in>
2025-07-14 18:06:49 +0530

    add cors

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 import userRouter from "./routes/user.route";
 import notesRouter from "./routes/notes.route";
@@ -8,6 +9,13 @@ import bookmarksRouter from "./routes/bookmark.route";
 import { errorHandler } from "./controllers/error.controller";
 
 const app = express();
+
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL || "http://localhost:3000",
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 app.use(cookieParser());
